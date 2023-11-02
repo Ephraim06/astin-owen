f7cf392cbdc7924ab57523a34bff5cf087480d83
Ephraim06 <esamanyayi95@gmail.com>
2023-11-02 19:28:09 +0200

    whatsapp feature and loader

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const plus = document.getElementById('plus')
-let counterPlus = 0;
-
-function increment(){
-    counterPlus = ++1;
-    plus.innerHTML = counterPlus
+// Spinner About
+var spinnerAbout = function () {
+    setTimeout(function () {
+        if ($('#spinner').length > 0) {
+            $('#spinner').removeClass('show');
+        }
+    }, 2000);
 };
+spinnerAbout();
 
-const sub = document.getElementById('sub')
-let counterSub = 0;
+//   POST WHATSAPP MESSAGE CONTENT
 
-function decrement(){
-    counterSub = --1;
-    sub.innerHTML = counterSub
-};
-decrement()
+function sendWhatsAppMessage() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    const mobileNumber = document.getElementById('phone').value;
+    const phoneNumber = '+27765627755';
+
+    const whatsappMessage = `Astin Owen | ZA: Enquiry%0A%0AName: ${name}%0APhone: ${mobileNumber}%0AEmail: ${email}%0AMessage: ${message}`;
+    const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${whatsappMessage}`;
+
+    window.location.href = whatsappURL;
+  }
